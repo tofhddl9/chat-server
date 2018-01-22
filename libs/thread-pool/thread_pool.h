83ba13c361dad053b8caf3363c37a8a0fe433c4c
tofhddl9 <tofhddl9@gmail.com>
2018-01-22 19:13:01 +0000

    Fix some types

--- libs/thread-pool/thread_pool.h
+++ libs/thread-pool/thread_pool.h
@@ -1,18 +1,20 @@
-#include <sys/types.h>
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
 
-#include <pthread.h>
-#include "../thread-safe-queue/thread_safe_queue.h"
+#include "thread_safe_queue.h"
 
 typedef void *(*job_t)(void *);
 
 typedef struct thread_pool {
-  thread_safe_queue jobs;
+  thread_safe_queue *jobs;
   pthread_t *threads;
   size_t thread_num;
   
 } thread_pool;
 
-thread_pool *create_thread_pool(size_t thread_num, size_t queue_size);
-void add_job_in_pool(thread_pool *tp, job_t func);
+void init_thread_pool(thread_pool *tp, size_t thread_num, size_t queue_size);
+void add_job_in_pool(thread_pool *tp, job_t job);
 
 void *thread_work(void *pool);
+
+#endif
