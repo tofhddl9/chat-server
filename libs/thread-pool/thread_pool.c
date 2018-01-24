6d5c57101f8bb7b47f18301726aff5d4b2bfdf3d
tofhddl9 <tofhddl9@gmail.com>
2018-01-24 23:36:02 +0000

    Add struct job and apply

--- libs/thread-pool/thread_pool.c
+++ libs/thread-pool/thread_pool.c
@@ -4,25 +4,24 @@
 
 #include <pthread.h>
 
-void init_thread_pool(thread_pool *tp, size_t thread_num, size_t queue_size)
+void init_thread_pool(thread_pool *tp, size_t th_num, size_t q_size)
 {
   int i;
-  tp->thread_num = thread_num;
-  tp->threads = (pthread_t *)malloc(sizeof(pthread_t) * thread_num);
+  tp->thread_num = th_num;
+  tp->threads = (pthread_t *)malloc(sizeof(pthread_t) * th_num);
 
   tp->jobs = (thread_safe_queue *)malloc(sizeof(thread_safe_queue));
-  tp->jobs->queue = (void **)malloc(sizeof(void *) * queue_size);
-  init_queue(tp->jobs, queue_size);
+  init_queue(tp->jobs, q_size);
 
-  for (i = 0; i < tp->thread_num; ++i) {
+  for (i = 0; i < tp->th_num; ++i) {
     pthread_create(&tp->threads[i], NULL, thread_work, (void *)tp);
   }
 }
 
-void add_job_in_pool(thread_pool *tp, job_t job)
+void add_job_in_pool(thread_pool *tp, job_t *job)
 {
-  int status = enqueue(tp->jobs, job);
-  if (status < 0)
+  int status = enqueue(tp->jobs, (char*)job);
+  if (status != 0)
     printf("queue is full\n");
 }
 
@@ -50,12 +49,13 @@ void free_thread_pool_with_join(thread_pool *tp)
 
 void *thread_work(void *pool)
 {
-  void (*fp)() = NULL;
+  job_t job;
+  char ret;
+
   thread_pool *tp = (thread_pool *)pool;
   printf("thread work hard\n");
   while (1) {
-    fp = dequeue(tp->jobs);
-    if(fp != NULL)
-      fp();
+    ret = dequeue(&tp.jobs, &job);
+    job.func(job.args);
   }
 }
--- libs/thread-pool/thread_pool.h
+++ libs/thread-pool/thread_pool.h
@@ -3,16 +3,21 @@
 
 #include "thread_safe_queue.h"
 
-typedef void *(*job_t)(void *);
+typedef void *(*f_ptr)(void *);
+
+typedef struct job {
+  f_ptr func;
+  void *args;
+} job;
 
 typedef struct thread_pool {
-  thread_safe_queue *jobs;
+  thread_safe_queue jobs;
   pthread_t *threads;
   size_t thread_num;
   
 } thread_pool;
 
-void init_thread_pool(thread_pool *tp, size_t thread_num, size_t queue_size);
+void init_thread_pool(thread_pool *tp, size_t th_num, size_t q_size);
 void add_job_in_pool(thread_pool *tp, job_t job);
 void free_thread_pool(thread_pool *tp);
 void free_thread_pool_with_join(thread_pool *tp);
