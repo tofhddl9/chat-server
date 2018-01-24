570f9284d342dfe2cc5412f15ae4b8726848a826
tofhddl9 <tofhddl9@gmail.com>
2018-01-24 14:54:12 +0000

    Redesign queue for general data

--- libs/thread-safe-queue/thread_safe_queue.h
+++ libs/thread-safe-queue/thread_safe_queue.h
@@ -4,10 +4,11 @@
 #include <sys/types.h>
 
 typedef struct thread_safe_queue {
-  void **queue;
+  char *queue;
   size_t head;
   size_t tail;
-  size_t size;
+  size_t queue_size;
+  size_t element_size;
   char _lock;
 
 } thread_safe_queue;
@@ -15,10 +16,10 @@ typedef struct thread_safe_queue {
 void lock(char *sq);
 void unlock(char *sq);
 
-void init_queue(thread_safe_queue *sq, size_t q_size);
+void init_queue(thread_safe_queue *sq, size_t e_size, size_t q_size);
 char is_empty(thread_safe_queue *sq);
 char is_full(thread_safe_queue *sq);
-char enqueue(thread_safe_queue *sq, void *item);
-void *dequeue(thread_safe_queue *sq);
+char enqueue(thread_safe_queue *sq, char *item);
+char dequeue(thread_safe_queue *sq, char *out);
 
 #endif
