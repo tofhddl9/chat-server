1f3a8ffab1427b8092a74977fa1eab48f6b5ec29
tofhddl9 <tofhddl9@gmail.com>
2018-01-27 14:18:06 +0000

    Design I/O APIs with epoll

--- libs/io/io.h
+++ libs/io/io.h
@@ -1,7 +1,16 @@
 #ifndef IO_H
 #define IO_H
 
-void init_io();
-void register_sock();
+struct io_manager {
+  int epoll_fd;
+  int listen_fd;
+  size_t max_event_num;
+  struct epoll_event *events;
+  struct thread_pool tp;
+}
 
+void init_io(struct io_manager *io_m, size_t thread_num, size_t max_event_num);
+void register_sock();
+void delete_sock();
+void do_io();
 #endif
