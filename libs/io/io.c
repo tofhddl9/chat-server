d97a54eda652a7e7bef79e40d4bb622b4b6dc562
tofhddl9 <tofhddl9@gmail.com>
2018-01-27 14:21:26 +0000

    Implement init_io()

--- libs/io/io.c
+++ libs/io/io.c
@@ -1,4 +1,32 @@
 #include "io.h"
+#include <sys/epoll.h>
+#include "thread-pool/thread-pool.h"
 
-void init_io();
-void register_sock();
+void init_io(struct io_manager *io_m, size_t thread_num, size_t max_event_num)
+{
+  struct epoll_event event = {EPOLLIN, listen_fd};
+
+  io_m->epoll_fd = epoll_create(1);
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &event); 
+ 
+  io_m->max_event_num = max_event_num;
+
+  io_m->events = (struct epoll_event *)
+    malloc(sizeof(epoll_event) * max_event_num);
+  
+  init_thread_pool(&io.m->tp, thread_num, element_size, max_event_num / 2);
+  
+}
+
+void register_sock(struct io_manager *io_m, int fd, uint8_t is_listen_fd)
+{
+}
+
+void delete_sock(struct io_manager *io_m, int fd)
+{
+}
+
+void do_io(struct io_manager *io_m, listen_sock_handler, client_sock_handler)
+{
+}
+
