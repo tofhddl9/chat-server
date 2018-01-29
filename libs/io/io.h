75229535d33682c4205d77ff1c2bb512f55bb392
tofhddl9 <tofhddl9@gmail.com>
2018-01-29 12:32:49 +0000

    Change data type of handler from int to void *

--- libs/io/io.h
+++ libs/io/io.h
@@ -1,19 +1,19 @@
 #ifndef IO_H
 #define IO_H
 
-#include "thread-pool/thread-pool.h"
+#include "thread_pool.h"
 
 struct io_manager {
   int epoll_fd;
   size_t max_event_num;
   struct epoll_event *events;
   struct thread_pool tp;
-  void (**handlers)(int);
-}
+  void (**handlers)(void *);
+};
 
 void init_io(struct io_manager *io_m, size_t thread_num,
   size_t max_event_num, size_t handler_num);
-void register_handler(struct io_manager *io_m, int id, void (*handler)(int));
+void register_handler(struct io_manager *io_m, int id, void (*handler)(void *));
 void register_sock(struct io_manager *io_m, int fd, int handler_id);
 void delete_sock(struct io_manager *io_m, int fd);
 void do_io(struct io_manager *io_m);
