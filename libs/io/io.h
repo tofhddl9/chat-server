aee4ed09fe1a49c9c2f2da4e0615b3658bb96e38
tofhddl9 <tofhddl9@gmail.com>
2018-01-29 09:57:05 +0000

    Add handler table in io_manager

--- libs/io/io.h
+++ libs/io/io.h
@@ -1,16 +1,20 @@
 #ifndef IO_H
 #define IO_H
 
+#include "thread-pool/thread-pool.h"
+
 struct io_manager {
   int epoll_fd;
-  int listen_fd;
   size_t max_event_num;
   struct epoll_event *events;
   struct thread_pool tp;
+  void (**handlers)(int);
 }
 
-void init_io(struct io_manager *io_m, size_t thread_num, size_t max_event_num);
-void register_sock(struct io_manager *io_m, int fd, int8_t fd_type, );
+void init_io(struct io_manager *io_m, size_t thread_num,
+  size_t max_event_num, size_t handler_num);
+void register_handler(struct io_manager *io_m, int id, void (*handler)(int));
+void register_sock(struct io_manager *io_m, int fd, int handler_id);
 void delete_sock(struct io_manager *io_m, int fd);
-void do_io(struct io_manager *io_m, listen_sock_handler, client_sock_handler);
+void do_io(struct io_manager *io_m);
 #endif
