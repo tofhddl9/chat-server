ef404c85167c85d3808b7179cbd8d611a76b686c
tofhddl9 <tofhddl9@gmail.com>
2018-01-29 12:37:12 +0000

    Seperate register_sock and register_handler. thereafter, re-implement do_io

--- libs/io/io.c
+++ libs/io/io.c
@@ -1,55 +1,66 @@
-#include "io.h"
+#include <stdlib.h>
+#include <unistd.h>
+
 #include <sys/epoll.h>
-#include "thread-pool/thread-pool.h"
 
-void init_io(struct io_manager *io_m, size_t thread_num, size_t max_event_num)
+#include "io.h"
+
+void init_io(struct io_manager *io_m, size_t thread_num,
+  size_t max_event_num, size_t handler_num)
 {
-  struct epoll_event event = {EPOLLIN, listen_fd};
-
-  io_m->epoll_fd = epoll_create(1);
-  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &event); 
+  io_m->epoll_fd = epoll_create(max_event_num);
  
   io_m->max_event_num = max_event_num;
 
   io_m->events = (struct epoll_event *)
-    malloc(sizeof(epoll_event) * max_event_num);
+    malloc(sizeof(struct epoll_event) * max_event_num);
   
-  init_thread_pool(&io.m->tp, thread_num, element_size, max_event_num / 2);
-  
+  init_thread_pool(&io_m->tp, thread_num, sizeof(job), max_event_num / 2);
+
+  io_m->handlers = (void (**)(void *))
+    malloc(sizeof(void (*)(void *)) * handler_num);
 }
 
-void register_sock(struct io_manager *io_m, int fd, uint8_t is_listen_fd)
+void register_handler(struct io_manager *io_m, int id, void (*handler)(void *))
 {
-  struct epoll_event read_event = {EPOLLIN, listen_fd};
-  
-  if (is_listen_fd)
-    io_m->listen_fd = fd;
+  io_m->handlers[id] = handler;
+}
+
+void register_sock(struct io_manager *io_m, int fd, int handler_id)
+{
+  uint64_t id_ = handler_id;
+
+  struct epoll_event read_event;
+  read_event.events = EPOLLIN;
+  read_event.data.ptr = (void *)((id_ << 32) + fd);
 
   epoll_ctl(io_m->epoll_fd, EPOLL_CTL_ADD, fd, &read_event);
 }
 
+
 void delete_sock(struct io_manager *io_m, int fd)
 {
-  epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+  epoll_ctl(io_m->epoll_fd, EPOLL_CTL_DEL, fd, NULL);
   close(fd);
 }
 
-void do_io(struct io_manager *io_m, listen_sock_handler, client_sock_handler)
+void do_io(struct io_manager *io_m)
 {
   int i, event_num;
+  uint32_t id, fd;
+  job work;
 
   while (1) {
-    event_num = epoll_wait(io_m->epoll_fd,
-        io_m->events, io_m->max_event_num, -1);
+    event_num =
+      epoll_wait(io_m->epoll_fd,io_m->events, io_m->max_event_num, -1);
 
     for (i = 0; i < event_num; ++i) {
-      if (events[i].data.fd == listen_fd) {
-        add_job_in_pool(io_m->tp, listen_sock_handler);
-      }
-      else if (events.data.fd == client_fd) {
-        add_job_in_pool(io_m->tp, client_sock_handler);
-      }
+      id = (uint64_t)io_m->events[i].data.ptr >> 32;
+      fd = (uint64_t)io_m->events[i].data.ptr & 0x00000000FFFFFFFF;
+      work.func = io_m->handlers[id];
+      work.args = (void *)&fd;
+
+      add_job_in_pool(&io_m->tp, &work);
     }
-    // problem : type of 2nd param of 'add_job_in_pool' is struct job..{func, args}
   }
 }
